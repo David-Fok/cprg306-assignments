5221e06d923e92000d97646c70c4f55005b7d6d4
David-Fok <foktatchun@gmail.com>
2024-01-17 19:21:35 -0700

    finished week 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <p>Tat Chun Fok</p>
+      <Link href="https://github.com/David-Fok/cprg306-assignments">
+        My GitHub repository
+      </Link>
+    </main>
+  );
+}
