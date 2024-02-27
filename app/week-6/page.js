7a12639a86374d28fd5d2cd6f14f2254fabbd63b
David-Fok <foktatchun@gmail.com>
2024-02-27 15:24:18 -0700

    week5

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -1,3 +1,13 @@
+import ItemList from "./item-list";
+import NewItem from "./new-item";
+import itemsData from items.json
+
+
 export default function Page() {
-  return <main></main>;
+  return (
+    <main className="bg-blue-100">
+      <h1 className="text-4xl font-bold text-red-950 mb-3">Shopping List</h1>
+      <ItemList />
+    </main>
+  );
 }
